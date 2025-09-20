2ab72b09362606747a8db4dace50147cb913ab26
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-09-20 20:29:57 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import PageHeader from "./components/layout/PageHeader"
+
+
+function App() {
+
+  return (
+    <div className="flex flex-col max-h-screen">
+      <PageHeader />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/layout/PageHeader.tsx
@@ -0,0 +1,8 @@
+
+function PageHeader() {
+  return (
+    <div>PageHeader</div>
+  )
+}
+
+export default PageHeader
